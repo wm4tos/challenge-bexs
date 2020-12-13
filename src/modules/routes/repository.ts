6b4eaad7bbb4c007d12ee1d953d463b17fea0b74
Wesley Matos <wrickee@gmail.com>
2020-12-13 05:10:09 -0300

    feat: write add route repository function

--- src/modules/routes/repository.ts
+++ src/modules/routes/repository.ts
@@ -1,4 +1,4 @@
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
 import { ROUTE_PATH } from 'src/common/config';
 
@@ -6,4 +6,10 @@ export const getRoutes = (): string => {
   const data = readFileSync(join(__dirname, '..', '..', '..', ROUTE_PATH), 'utf-8');
 
   return data;
+};
+
+export const addRoute = (value: string): void => {
+  const routes = getRoutes();
+
+  writeFileSync(join(__dirname, '..', '..', '..', ROUTE_PATH), routes.concat('\r\n', value), 'utf-8');
 };
