d1440a36b20e407ad4a2f9964172255749caab04
Wesley Matos <wrickee@gmail.com>
2020-12-13 05:11:05 -0300

    feat: use cors and body parser

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import { PORT } from 'src/common/config';
 import express, { Router } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
+
+app.use(cors({
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+app.use(bodyParser.text());
 
 app.use('/api', routes(Router()));
 
