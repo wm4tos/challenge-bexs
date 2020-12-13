713b197395fc2ea10f9f6d3da4e0ae49c67cec96
Wesley Matos <wrickee@gmail.com>
2020-12-13 05:10:32 -0300

    feat: write controller write route function

--- src/modules/routes/controller.ts
+++ src/modules/routes/controller.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from 'express';
-import { getRoute } from './service';
+import { addNewRoute, getRoute } from './service';
 
 export const getRoutePath = (req: Request, res: Response): any => {
   const { query: { to, from } } = req;
 
   return res.send(getRoute(from as string, to as string));
+};
+
+export const newRouteToCsv = (req: Request, res: Response): any => {
+  const { body } = req;
+
+  return res.send(addNewRoute(body));
 };
